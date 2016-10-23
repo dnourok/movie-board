8f1c5fb3b4d3c24cb28d77a45e2b936dddfa90a3
Danielle Nourok <dnourok1@gmail.com>
2016-10-23 09:19:43 -0400

    linked stylesheet and cleaned up packages not needed

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var request = require('request');
 
+
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
+});
+
+app.use('/', function(req, res){
+  res.sendfile('style.css');
 });
 
 http.listen(8080, function(){
