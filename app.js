61a1619f01cca0f605a74654cc9682381e9940bd
Danielle Nourok <dnourok1@gmail.com>
2016-10-26 09:11:29 -0400

    changed file path for css and js so now are both in public folder

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
 var request = require('request');
 var fs = require('fs');
 var middleware = require('middleware');
+var path = require('path');
+var app = express();
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-app.use('/', function(req, res){
-  res.sendfile('./src/index.js');
-});
-
-app.use('/', function(req, res){
-  res.sendfile('./css/style.css');
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 
-http.listen(8080, function(){
+app.listen(8080, function(){
   console.log('listening on *:8080');
 });
